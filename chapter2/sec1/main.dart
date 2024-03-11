eb53283909e22518987ccbfa51dcad4163ebd7e5
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-03-11 16:31:13 +0900

    色んな初期化の遅延について実装してみた

--- chapter2/sec1/main.dart
+++ chapter2/sec1/main.dart
@@ -2,6 +2,9 @@ import 'var_dec.dart';
 
 void main() {
   // List2_1_1();
-  List2_1_2();
+  // List2_1_2();
   // List2_1_3();
+  // List2_1_4();
+  // List2_1_5();
+  List2_1_6();
 }
--- chapter2/sec1/var_dec.dart
+++ chapter2/sec1/var_dec.dart
@@ -37,3 +37,43 @@ void List2_1_2() {
 //   // Error: Final variable 'number' must be assigned before it can be used.
 //   print(number);
 // }
+
+void List2_1_4() {
+  // 初期化を遅延させる
+  late String globalVariable;
+
+  void initializeGlobalVariable() {
+    globalVariable = '初期化';
+    print(globalVariable);
+  }
+
+  initializeGlobalVariable();
+}
+
+void List2_1_5() {
+  // 初期化を遅延させて、一度初期化さえたら変更不可にさせる
+  late final String globalName;
+
+  void initializeGlobalName() {
+    globalName = '太郎';
+    // globalName = '次郎'; // Error: Can't assign to the final variable 'globalName'.
+    print(globalName);
+  }
+
+  initializeGlobalName();
+}
+
+void List2_1_6() {
+  // 処理の重い関数を定義
+  String highCostFunction() {
+    return '高コストな処理';
+  }
+
+  // 上記関数の結果を代入させることで初期値が与えられるので、
+  // variableが呼ばれるまで初期化を遅らせる
+  // こうすることで後続の処理の負担を下げることができる
+  late String variable = highCostFunction();
+
+  // ここで初期化される
+  print(variable);
+}
