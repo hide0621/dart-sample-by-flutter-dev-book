391c5e83928938ba01c66a5a33e8cc6ac1c206fc
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-03-11 16:04:58 +0900

    色んな初期値の与え方について実装してみた

--- chapter2/sec1/main.dart
+++ chapter2/sec1/main.dart
@@ -1,5 +1,7 @@
 import 'var_dec.dart';
 
 void main() {
-  List2_1();
+  // List2_1_1();
+  List2_1_2();
+  // List2_1_3();
 }
--- chapter2/sec1/var_dec.dart
+++ chapter2/sec1/var_dec.dart
@@ -1,11 +1,39 @@
-var age = 0;
+void List2_1_1() {
+  var age = 0;
 
-final num = 10;
+  final num = 10;
 
-const num2 = 20;
+  const num2 = 20;
 
-void List2_1() {
   print(age);
   print(num);
   print(num2);
 }
+
+void List2_1_2() {
+  final flag = DateTime.now().hour.isEven;
+
+  final int number; // あとで初期化もできる
+
+  if (flag) {
+    number = 10;
+  } else {
+    number = 20;
+  }
+  print(number);
+}
+
+// void List2_1_3() {
+//   final name = '太郎';
+//
+//   final int number; // あとで初期化もできる
+//
+//   if (name == '次郎') {
+//     number = 10;
+//   } else if (name == '三郎') {
+//     number = 20;
+//   }
+//   // 利用時までに初期化がなされないのならエラーが出る
+//   // Error: Final variable 'number' must be assigned before it can be used.
+//   print(number);
+// }
