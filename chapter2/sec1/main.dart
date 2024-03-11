0425b333cf6feef28495b53fd6b98c6ea19f0851
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-03-11 13:52:50 +0900

    第２章の最初のコミット（変数と定数の宣言）

---
+++ chapter2/sec1/main.dart
@@ -0,0 +1,5 @@
+import 'var_dec.dart';
+
+void main() {
+  List2_1();
+}
---
+++ chapter2/sec1/var_dec.dart
@@ -0,0 +1,11 @@
+var age = 0;
+
+final num = 10;
+
+const num2 = 20;
+
+void List2_1() {
+  print(age);
+  print(num);
+  print(num2);
+}
