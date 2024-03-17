0ad249ec5df0a8a1e5b73e0a0e4e4fa929ad60ef
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-03-17 09:34:42 +0900

    固定長リストを実装してみた

--- chapter2/sec2/built_in_type.dart
+++ chapter2/sec2/built_in_type.dart
@@ -9,3 +9,26 @@ void List2_2_1() {
 
   print(str2);
 }
+
+void List2_2_2() {
+  // リスト（スライスのようなもの）を定義 → 可変長
+  final baseList = [0, 1, 2, 3];
+
+  print(baseList);
+
+  baseList.add(4);
+
+  print(baseList);
+
+  baseList.remove(4);
+
+  print(baseList);
+
+  // 固定長のリストを新しく定義する
+  final fixedLenghtList = List.unmodifiable(baseList);
+
+  print(fixedLenghtList);
+
+  // 以下のコードはエラーになる
+  // fixedLenghtList.add(5); // Unhandled exception: Unsupported operation: Cannot add to an unmodifiable list
+}
--- chapter2/sec2/main.dart
+++ chapter2/sec2/main.dart
@@ -1,5 +1,6 @@
 import 'built_in_type.dart';
 
 void main() {
-  List2_2_1();
+  // List2_2_1();
+  List2_2_2();
 }
