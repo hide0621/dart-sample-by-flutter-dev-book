5d0955cb66e2210e940b16b3db05debe50858c8d
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-03-20 11:03:58 +0900

    ジェネリックな関数を実装した

--- chapter2/sec3/generics.dart
+++ chapter2/sec3/generics.dart
@@ -1,3 +1,4 @@
+// ジェネリックなクラスを定義
 class Foo<T> {
   T _value;
   Foo(this._value);
@@ -12,4 +13,22 @@ void List2_3_1() {
 
   var stringFoo = Foo<String>('Hello, World!');
   print(stringFoo.value); // Hello, World!
+}
+
+// ジェネリックな関数を定義
+T? firstOrNull<T>(List<T> items) {
+  if (items.isEmpty) {
+    return null;
+  }
+  return items.first;
+}
+
+void List2_3_2() {
+  var numbers = [1, 2, 3];
+  var firstNumber = firstOrNull(numbers);
+  print(firstNumber); // 1
+
+  var emptyList = <int>[];
+  var firstEmpty = firstOrNull(emptyList);
+  print(firstEmpty); // null
 }
