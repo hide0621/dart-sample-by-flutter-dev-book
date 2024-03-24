085f6cd8fb718d02fbdff0383f7dcc7d27f1e583
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-03-25 08:40:43 +0900

    パターン構文のうち、マッチングと分解宣言のパターン構文（Mapの場合）のサンプルコードを実装した

--- chapter2/sec6/Matching_and_Decomposition_Declaration.dart
+++ chapter2/sec6/Matching_and_Decomposition_Declaration.dart
@@ -5,5 +5,15 @@
 // ...を使うことで、要素数を合わせることができる
 void List2_6_8() {
   final [a, b, ..., c] = [1, 2, 3, 4, 5];
-  print('a: $a, b: $b, c: $c');
+  print('a: $a, b: $b, c: $c'); // a: 1, b: 2, c: 5
+}
+
+// Mapの場合
+// 分解宣言をさせるためには、キーが同じ必要がある（ただし全てのキーとマッチさせる必要はない）
+void List2_6_9() {
+  final {
+    200: successful,
+    404: notFound
+  } = {200: 'OK', 404: 'Not Found', 500: 'Internal Server Error'};
+  print('200: $successful, 404: $notFound'); // 200: OK, 404: Not Found
 }
