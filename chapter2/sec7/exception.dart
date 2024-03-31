20981670301e2f62221d66392df32dc2f28b5715
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-04-01 08:53:30 +0900

    MyExceptionクラスにtoStringメソッドをオーバーライドさせ、例外補足時のエラー文をカスタムしてみた

--- chapter2/sec7/exception.dart
+++ chapter2/sec7/exception.dart
@@ -2,12 +2,26 @@ void doSomething() {
   throw MyException('エラーが発生しました');
 }
 
+// MyExceptionがtoStringメソッドをオーバライドしていない場合
+// class MyException {
+//   MyException(this.message);
+//   final String message;
+// }
+
+// MyExceptionがtoStringメソッドをオーバライドした場合
 class MyException {
   MyException(this.message);
   final String message;
+
+  @override
+  String toString() {
+    return 'MyException: $message';
+  }
 }
 
 // 例外処理の基本形
+// 実行結果（toStringをオーバーライドしてない時）: Instance of 'MyException'
+// 実行結果（toStringをオーバーライドした時）: MyException: エラーが発生しました
 void List2_7_1() {
   try {
     doSomething();
