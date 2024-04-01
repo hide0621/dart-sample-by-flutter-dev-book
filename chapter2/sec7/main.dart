100a9ce4de76143ab8fc762cfbfa8f02cb4440f7
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-04-02 08:36:33 +0900

    再スローされた例外をハンドリングしてみた

--- chapter2/sec7/main.dart
+++ chapter2/sec7/main.dart
@@ -5,5 +5,12 @@ void main() {
   // List2_7_2();
   // List2_7_3();
   // List2_7_4();
-  List2_7_5();
+
+  // 例外の再スローを補足して、その例外を処理する方法
+  try {
+    List2_7_5(); // catch : Instance of 'MyException'
+  } on MyException catch (e) {
+    print(
+        'Caught rethrown exception: $e'); // Caught rethrown exception: Instance of 'MyException'
+  }
 }
