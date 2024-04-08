d26f1e355396bebe5620c8a977949e8a66281f90
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-04-09 08:25:49 +0900

    dartでもコメントの書式を学んだ

--- chapter2/sec8/main.dart
+++ chapter2/sec8/main.dart
@@ -1,3 +1,13 @@
 void main() {
-  print('Hello, World!');
+  doubleValue(10);
 }
+
+// 引数を２倍にする
+int doubleValue(int value) {
+  return value * 2;
+}
+
+/*
+  以下のように書くこともできる
+  int doubleValue(int value) => value * 2;
+*/
