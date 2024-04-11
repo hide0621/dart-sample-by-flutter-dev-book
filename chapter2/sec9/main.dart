4a3ff1c82260329a2a12428c7949e2183bc6a7e0
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-04-12 08:31:32 +0900

    タイププロモーションのサンプルを書いた

--- chapter2/sec9/main.dart
+++ chapter2/sec9/main.dart
@@ -3,5 +3,6 @@ import 'null.dart';
 void main() {
   // List2_9_1();
   // List2_9_2();
-  List2_9_3();
+  // List2_9_3();
+  List2_9_4();
 }
--- chapter2/sec9/null.dart
+++ chapter2/sec9/null.dart
@@ -33,3 +33,17 @@ void List2_9_3() {
   print(str2!
       .length); // 5 // Warning: Operand of null-aware operation '!' has type 'String' which excludes null.
 }
+
+// タイププロモーション
+// タイププロモーションとは、Dartの機能の一つで、特定の条件下で変数の型を自動的に「昇格」させること
+// 例えば、null許容型の変数がnullでないことが確定した場合、その変数をnull許容型から非null許容型に変換することができる
+void List2_9_4() {
+  String? str;
+  str = 'Hello';
+  if (str == null) {
+    return;
+  }
+  // 上記のif文を通過した時点で、Dartのコンパイラはstrがnullでないことを知っているため、
+  // その後のコードではstrをnullでないString型として扱う（タイププロモーション）
+  print(str.length);
+}
