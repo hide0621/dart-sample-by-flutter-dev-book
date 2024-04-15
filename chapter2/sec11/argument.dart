5a86e0b04bf66733378a7de538bf428194866bc3
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-04-15 09:37:11 +0900

    関数の名前付き引数のサンプルを書いた

--- chapter2/sec11/argument.dart
+++ chapter2/sec11/argument.dart
@@ -26,3 +26,13 @@ void List2_11_2() {
   makeColor(0xFF, 0x00, 0x33,
       0x88); // 引数alphaを与えて呼び出し red: 255, green: 0, blue: 51, alpha: 136
 }
+
+// 名前付き引数
+void List2_11_3() {
+  void makeColor({int? red, int? green, int? blue}) {
+    print('red: $red, green: $green, blue: $blue');
+  }
+
+  // 呼び出し時に引数の名前を指定する
+  makeColor(red: 0xFF, green: 0x00, blue: 0x33); // red: 255, green: 0, blue: 51
+}
--- chapter2/sec11/main.dart
+++ chapter2/sec11/main.dart
@@ -1,6 +1,7 @@
 import 'argument.dart';
 
 void main() {
-  List2_11_1();
-  List2_11_2();
+  // List2_11_1();
+  // List2_11_2();
+  List2_11_3();
 }
