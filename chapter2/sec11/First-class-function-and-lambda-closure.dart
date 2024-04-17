abfa68345b6bed20da4f97e6fbae77533b456bda
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-04-18 08:53:13 +0900

    関数オブジェクトについてコメントを追記

--- chapter2/sec11/First-class-function-and-lambda-closure.dart
+++ chapter2/sec11/First-class-function-and-lambda-closure.dart
@@ -1,4 +1,6 @@
 /// 第１級関数と匿名関数
+///
+/// 関数オブジェクトの型は、「戻り値の型 Function(引数の型)」という形式で表現される。
 
 void List2_11_7() {
   // 引数を２倍にして返す関数
@@ -6,7 +8,7 @@ void List2_11_7() {
     return value * 2;
   }
 
-  // 関数doubleValueを変数fに代入
+  // 関数doubleValueを変数fに代入 → 変数fはint型の引数を受け取り、int型の戻り値を返す関数オブジェクトとなる
   final int Function(int) f = doubleValue;
 
   final result = f(10);
