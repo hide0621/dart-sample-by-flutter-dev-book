e80f1849e1abe4f02c869baf1772ff3b26fc6639
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-04-18 08:31:04 +0900

    アロー関数のサンプルを書いた

--- chapter2/sec11/main.dart
+++ chapter2/sec11/main.dart
@@ -1,9 +1,11 @@
-import 'argument.dart';
+import 'Arrow-function.dart';
+// import 'argument.dart';
 
 void main() {
   // List2_11_1();
   // List2_11_2();
   // List2_11_3();
   // List2_11_4();
-  List2_11_5();
+  // List2_11_5();
+  List2_11_6();
 }
