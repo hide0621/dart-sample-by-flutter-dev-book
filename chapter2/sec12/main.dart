338c1b9d9b4d727c3689ddc34ce2443d54e3816d
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-04-20 10:38:27 +0900

    先のコミットにコメントを追記

--- chapter2/sec12/main.dart
+++ chapter2/sec12/main.dart
@@ -8,7 +8,9 @@ void main() {
   // List2_12_4();
   List2_12_5();
 
-  // 以下はprivateなのでエラー
+  /// 以下はprivate化されているものを呼び出しているのでエラーとなる
+  /// private化されているものは同じファイル内でしか呼び出せない
+  /// 他のファイルから呼び出す場合は、ゲッターを介して呼び出す
   // final user = User(1, 'password');
   // print(user._password);
 }
