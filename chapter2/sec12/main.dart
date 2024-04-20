219c5bafbfe205cdbca00cbc6a4f53953db04a55
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-04-20 10:35:20 +0900

    ゲッターとセッターのサンプルを書いてみた

--- chapter2/sec12/main.dart
+++ chapter2/sec12/main.dart
@@ -1,7 +1,14 @@
-import 'Orthodox-class-pattern.dart';
+import 'Getter-and-Setter.dart';
+// import 'Orthodox-class-pattern.dart';
 
 void main() {
   // List2_12_1();
   // List2_12_2();
-  List2_12_3();
+  // List2_12_3();
+  // List2_12_4();
+  List2_12_5();
+
+  // 以下はprivateなのでエラー
+  // final user = User(1, 'password');
+  // print(user._password);
 }
