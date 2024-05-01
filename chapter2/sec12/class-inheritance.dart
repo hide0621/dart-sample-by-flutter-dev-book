12208241062195ee6a9509b62a345f846b6ab581
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-05-02 08:36:16 +0900

    superキーワードを使ってスーパークラスを参照してみた

--- chapter2/sec12/class-inheritance.dart
+++ chapter2/sec12/class-inheritance.dart
@@ -1,3 +1,4 @@
+/// 最もシンプルなクラス継承のパターン
 class Animal {
   String greet() => 'Hello';
 }
@@ -7,4 +8,18 @@ class Dog extends Animal {}
 void List2_12_10() {
   final dog = Dog();
   print(dog.greet()); // Hello
+}
+
+/// superキーワードを使ってスーパークラスを参照するパターン
+class Animal2 {
+  String greet() => 'Hello';
+}
+
+class Dog2 extends Animal2 {
+  String sayHello() => super.greet();
+}
+
+void List2_12_11() {
+  final dog = Dog2();
+  print(dog.sayHello()); // Hello
 }
