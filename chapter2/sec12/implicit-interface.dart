56adc50c1752e97f68217df11015e546db7fe1e1
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-05-04 06:42:24 +0900

    Dartのインターフェースがメソッドだけでなく、フィールドもオーバーライド（具象側でそのフィールドを使ったコンストラクタを定義）しないといけない旨を表すコードを書いてみた

--- chapter2/sec12/implicit-interface.dart
+++ chapter2/sec12/implicit-interface.dart
@@ -1,6 +1,8 @@
 /// Dartでは全てのクラスは暗黙的にインターフェースが定義されている
 /// implementsキーワードに続けて、インターフェースとして実装する型名を記述することで、そのクラスがそのインターフェースを実装していることを示す
 class Animal {
+  // Animal(this.name);
+  // final String name;
   String greet() => 'Hello';
   // void eat() {
   //   print('Animal eats');
@@ -9,10 +11,21 @@ class Animal {
 
 /// 上記インターフェースのeatメソッドがコメントアウト化されていなかったら、このメソッドもオーバーライドしないといけない
 /// そうしないと、「Missing concrete implementation of 'Animal.eat'.」というエラーが出る
+/// これはnameフィールドの場合も同様で、オーバーライドをしないと「Missing concrete implementation of 'getter Animal.name'」というエラーが出る
 /// そのため、インターフェースのメソッドが多い場合は、インターフェースを使うことで、オーバーライドの漏れを防げる（実装の強制）
+/// 全てのインスタンスメンバ、メソッドをオーバーライドしないといけない点が、extendsキーワードとimplementsキーワードの違い
 class Dog implements Animal {
+  // @override
+  // Dog(this.name);
+  // final String name;
+
   @override
   String greet() => 'Bow-Bow';
+
+  // @override
+  // void eat() {
+  //   print('Dog eats');
+  // }
 }
 
 void List2_12_16() {
