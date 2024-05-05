a1af96a39bba348505cfc6658a6ff6545f6676fe
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-05-05 14:39:11 +0900

    先のコミットにコメントを追記

--- chapter2/sec12/extension-method.dart
+++ chapter2/sec12/extension-method.dart
@@ -1,8 +1,14 @@
 /// 拡張メソッド
 ///
+/// 拡張メソッドはメソッドのラップ、カスタムと同じものと思われる
+///
+/// extension <拡張名> on <拡張対象の型>
+/// <拡張対象の型>とは紐づくオブジェクト自身、型自身を指しているものと思われ、Go言語に似ている
+/// <拡張名>は識別子のようなものと思われる
 extension SwapList<T> on List<T> {
   // 引数のインデックスの要素を入れ替える拡張メソッド
   void swap(int index1, int index2) {
+    // thisキーワードは拡張対象の型を指す（紐づくオブジェクト自身）
     final tmp = this[index1];
     this[index1] = this[index2];
     this[index2] = tmp;
