bb58356242705612ef3500276e0fb580f392d02c
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-05-11 09:41:19 +0900

    高度なenumにゲッターを書いてみた

--- chapter2/sec12/enum2.dart
+++ chapter2/sec12/enum2.dart
@@ -1,5 +1,6 @@
 /// 高度なenumの書き方
 
+/// 以下はフィールドやメソッド、factoryコンストラクタ、constantコンストラクタを持つenumの書き方
 enum Shape1 {
   circle(corner: 0),
   triangle(corner: 3),
@@ -12,9 +13,13 @@ enum Shape1 {
   factory Shape1.ellipse() {
     return Shape1.circle;
   }
+
+  // ゲッターを書いてみた
+  int get getCorner => corner;
 }
 
 void List2_12_244() {
   final ellipse = Shape1.ellipse();
   print(ellipse.corner);
+  print(ellipse.getCorner);
 }
