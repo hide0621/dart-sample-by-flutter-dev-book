603309fe34d15792e7ee0e7a5faaf62c6cb04ceb
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-05-17 08:42:57 +0900

    final修飾子のついたクラスの特徴についてドキュメントコメントを書いた

--- chapter2/sec12/final/class-modifier-final.dart
+++ chapter2/sec12/final/class-modifier-final.dart
@@ -1,6 +1,10 @@
 /// final修飾子
 ///
+/// 特徴特徴（自身が宣言されたライブラリ以外での場合）← 同一ファイル以外での振る舞いについて
 ///
+/// インスタンス化 OK
+/// extendsキーワードによる継承 NG
+/// implementsキーワードによる実装 NG
 
 final class Animal {
   String greet() => 'hello';
