9b7a890583aa31b85dab833a2c16ce4bb8f3b176
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-05-17 09:04:11 +0900

    mixinを付与したクラスのサンプルを書いてみた

--- chapter2/sec12/main.dart
+++ chapter2/sec12/main.dart
@@ -9,7 +9,8 @@
 // import 'base/base-use.dart';
 // import 'base/class-modifier-base.dart';
 // import 'abstract_interface/use-abstract-interface.dart';
-import 'final/final-use.dart';
+// import 'final/final-use.dart';
+import 'mixin/class-modifier-mixin.dart';
 // import 'interface/interface-use.dart';
 // import 'class-modifier-abstract.dart';
 // import 'enum.dart';
@@ -77,5 +78,7 @@ void main() {
 
   // List2_12_30();
 
-  List2_12_31();
+  // List2_12_31();
+
+  List2_12_32();
 }
--- chapter2/sec12/mixin/class-modifier-mixin.dart
+++ chapter2/sec12/mixin/class-modifier-mixin.dart
@@ -1,3 +1,33 @@
 /// mixin修飾子
 ///
 ///
+
+mixin class Horse {
+  void run() {
+    print('run');
+  }
+}
+
+mixin Bird {}
+
+class Pegasus with Bird, Horse {}
+
+/// mixinと同様に、extendsは使えずコンストラクも宣言できない
+// mixin class Fish extends Bird {}
+// mixin class Fish extends Horse {}
+// mixin class Fish extends Pegasus {}
+
+/// これもできない（mixinはクラスではないので？）
+// class Fish extends Bird {}
+// mixin Fish extends Pegasus {}
+
+/// これはできる（mixinと同様？）
+class Pegasus2 extends Horse {
+  Pegasus2();
+}
+
+List2_12_32() {
+  /// mixinを付与したクラスはインスタンス化は可能
+  final horse = Horse();
+  horse.run();
+}
