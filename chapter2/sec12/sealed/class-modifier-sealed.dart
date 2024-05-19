10c5ef6e3137d41912bc4fec349eb0703109c862
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-05-20 08:28:27 +0900

    sealed修飾子が付与されたクラスは自身が属するライブラリではサブタイプ化ができる旨を表すコードを書いてみた

--- chapter2/sec12/sealed/class-modifier-sealed.dart
+++ chapter2/sec12/sealed/class-modifier-sealed.dart
@@ -9,3 +9,28 @@ void List2_12_33() {
   // final shape = Shape(); // Abstract classes can't be instantiated.
   print('sealed修飾子はabstract classとして扱われるため、インスタンス化はできない');
 }
+
+class Rectangle extends Shape {
+  @override
+  int corner = 4;
+}
+
+class Triangle extends Shape {
+  @override
+  int corner = 3;
+}
+
+class Circle extends Shape {
+  @override
+  int corner = 0;
+}
+
+void List2_12_34() {
+  final rectangle = Rectangle();
+  final triangle = Triangle();
+  final circle = Circle();
+
+  print('rectangle: ${rectangle.corner}');
+  print('triangle: ${triangle.corner}');
+  print('circle: ${circle.corner}');
+}
