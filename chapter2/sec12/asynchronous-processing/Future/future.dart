7de9a0769b7c5522e01ebecbe7bc76d3a2b0271f
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-05-23 18:51:58 +0900

    Future型の学習その１

--- chapter2/sec12/asynchronous-processing/Future/future.dart
+++ chapter2/sec12/asynchronous-processing/Future/future.dart
@@ -1,2 +1,13 @@
 /// 非同期処理
 /// Future型について
+
+import 'dart:io';
+
+void List2_12_36() {
+  Future<String> content = File('file.txt').readAsString();
+  content.then((content) => print(content));
+}
+
+void main() {
+  List2_12_36();
+}
