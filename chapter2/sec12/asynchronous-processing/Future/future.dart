ccd4f4591242fe858d3593553bf7cd6e9e5c08ff
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-05-23 18:59:39 +0900

    Future型の学習その２

--- chapter2/sec12/asynchronous-processing/Future/future.dart
+++ chapter2/sec12/asynchronous-processing/Future/future.dart
@@ -1,5 +1,9 @@
 /// 非同期処理
 /// Future型について
+///
+/// 特徴：
+/// - awaitキーワードはasync関数内でのみ使用可能
+/// - async関数の戻り値は暗黙的にFutureクラスでラップされる
 
 import 'dart:io';
 
@@ -8,6 +12,13 @@ void List2_12_36() {
   content.then((content) => print(content));
 }
 
+/// 上のコードをリファクタリングするとこうなる（コードの量が減り、読みやすくなる？）
+Future<void> List2_12_37() async {
+  String content = await File('file.txt').readAsString();
+  print(content);
+}
+
 void main() {
-  List2_12_36();
+  // List2_12_36();
+  List2_12_37();
 }
