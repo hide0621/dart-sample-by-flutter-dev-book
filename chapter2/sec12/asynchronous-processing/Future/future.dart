cb6edaa0ab9497e427c596a346c064dff27e7832
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-05-24 08:37:55 +0900

    Future型のエラーハンドリングをtry-catch文で書いてみた

--- chapter2/sec12/asynchronous-processing/Future/future.dart
+++ chapter2/sec12/asynchronous-processing/Future/future.dart
@@ -21,20 +21,32 @@ Future<void> List2_12_37() async {
 void main() {
   // List2_12_36();
   // List2_12_37();
-  List2_12_38();
+  // List2_12_38();
+  List2_12_39();
 }
 
-/// Future型のエラーハンドリング
+/// Future型のエラーハンドリング~対象の関数~
 Future<String> fetchUserName() {
   var str =
       Future.delayed(const Duration(seconds: 1), () => throw 'User not found');
   return str;
 }
 
+/// Future型のエラーハンドリング~その1~
 void List2_12_38() {
   fetchUserName().then((name) {
     print('User name is $name');
   }).catchError((e) {
     print(e);
   });
+}
+
+/// Future型のエラーハンドリング~その2~
+void List2_12_39() async {
+  try {
+    final name = await fetchUserName();
+    print('User name is $name');
+  } catch (e) {
+    print(e);
+  }
 }
