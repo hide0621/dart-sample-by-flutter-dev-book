d96b477d00522d037cd604f6dfa7776814ecb6c6
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-05-27 08:27:35 +0900

    Zoneを使わなかった場合の処理を書いてみた

--- chapter2/sec12/asynchronous-processing/Zone/zone.dart
+++ chapter2/sec12/asynchronous-processing/Zone/zone.dart
@@ -1,2 +1,30 @@
 /// 非同期処理
 /// Zoneについて
+
+/// Zoneを使わない場合
+Future<String> fetchUserName() {
+  var str =
+      Future.delayed(const Duration(seconds: 1), () => throw 'User not found');
+  return str;
+}
+
+/// これは例外を補足できるが...
+void List2_12_55() {
+  fetchUserName().then((name) {
+    print('User name is $name');
+  }).catchError((e) {
+    print(e); // User not found
+  });
+}
+
+/// こっちは出来ない...
+void List2_12_56() {
+  fetchUserName().then((name) {
+    print('User name is $name');
+  });
+}
+
+void main() {
+  // List2_12_55();
+  List2_12_56();
+}
